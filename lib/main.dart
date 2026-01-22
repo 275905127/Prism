dbe08b6ab6bee262a1970ca2a5463718b1db7c7e
Han <275905127@qq.com>
2026-01-22 13:12:16 +0800

    更新 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -55,8 +55,13 @@ class MyApp extends StatelessWidget {
           ),
         ),
 
-        // Source manager
-        ChangeNotifierProvider(create: (_) => SourceManager()),
+        // ✅ Source manager (no SharedPreferences)
+        ChangeNotifierProvider(
+          create: (ctx) => SourceManager(
+            prefs: ctx.read<PreferencesStore>(),
+            logger: ctx.read<PrismLogger>(),
+          ),
+        ),
 
         // WallpaperService (UI 唯一入口)
         ProxyProvider5<Dio, RuleEngine, PixivRepository, PreferencesStore, PrismLogger, WallpaperService>(
